a8aa8d045c53d51f02886d6edc201ab34d35258d
Yusufjon <yusupeu.dev@gmail.com>
2026-02-11 17:57:38 +0500

    binary classification using pytorch

--- 2.classification/1circles/data/processed.py
+++ 2.classification/1circles/data/processed.py
@@ -7,6 +7,14 @@ y = torch.from_numpy(y).type(torch.float)
 
 print("Types:", X.dtype, y.dtype, "\n", "-"*30)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 print("Lenghts:", len(X_train), len(X_test), len(y_train), len(y_test), "\n", "-"*30)
+print("Shapes:", X_train.shape, X_test.shape, y_train.shape, y_test.shape, "\n", "-"*30)
+
+# Move to device
+
+X_train = X_train.to("mps").squeeze()
+X_test = X_test.to("mps").squeeze()
+y_train = y_train.to("mps").squeeze()
+y_test = y_test.to("mps").squeeze()
--- 2.classification/1circles/train.py
+++ 2.classification/1circles/train.py
@@ -4,30 +4,73 @@ import torch.nn as nn
 from models.classify import CircleModelV1
 from data.processed import X_train, X_test, y_train, y_test
 
+epochs = 1000
+# Calculate accuracy (a classification metric)
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
+
+def train_loop(model: nn.Module, data, optimizer, loss_fn):
+    train_loss_values = []
+    test_loss_values = []
+    epoch_count = []
+    X_train, X_test, y_train, y_test = data
+    
+    for epoch in range(epochs):
+        model.train()
+        y_logits = model(X_train).squeeze()
+        y_pred = torch.round(torch.sigmoid(y_logits))
+
+
+        loss = loss_fn(y_pred, y_train)
+
+        acc = accuracy_fn(y_true=y_train, y_pred=y_pred)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        model.eval()  # Changed from model_0 to model
+        with torch.inference_mode():
+
+            test_logits = model(X_test).squeeze()
+            test_preds = torch.round(torch.sigmoid(test_logits))
+
+            
+
+            test_loss = loss_fn(test_preds, y_test)
+            test_acc = accuracy_fn(y_pred=test_preds, y_true=y_test)
+
+            test_loss = loss_fn(test_preds, y_test.type(torch.float))
+            
+            if epoch % 10 == 0:
+                epoch_count.append(epoch)
+                train_loss_values.append(loss.item())
+                test_loss_values.append(test_loss.item())
+                print(f"Train: {epoch}th epoch | Loss: {loss.item()} | Acc: {acc}")
+                print(f"Test: {epoch}th epoch | Loss: {test_loss.item()} | Acc: {test_acc}")
+    
+    return train_loss_values, test_loss_values, epoch_count
+
 ## device agnostic code
 device = "cpu"
 if torch.cuda.is_available(): device = 'cuda'
 if torch.mps.is_available(): device = 'mps'
 
-print("Device:", device)
 
-model0 = CircleModelV1().to(device)
-
-model1 = nn.Sequential(
-    nn.Linear(2,5),
-    nn.Linear(5,1)
-).to(device)
-
-print(model0, model1)
-
-with torch.inference_mode():
-    # Make predictions with the model
-    untrained_preds = model1(X_test.to(device))
-    print(f"Length of predictions: {len(untrained_preds)}, Shape: {untrained_preds.shape}")
-    print(f"Length of test samples: {len(y_test)}, Shape: {y_test.shape}")
-    print(f"\nFirst 10 predictions:\n{untrained_preds[:10]}")
-    print(f"\nFirst 10 test labels:\n{y_test[:10]}")
+model1 = CircleModelV1().to(device)
 
 
+
+optimizer = torch.optim.SGD(lr=0.001, params=model1.parameters())
+lossfn = nn.BCELoss()
+
+train_loss_values, test_loss_values, epoch_count = train_loop(
+    model1, (X_train, X_test, y_train, y_test), 
+    optimizer, lossfn
+)
+
+print(train_loss_values, test_loss_values, epoch_count)
 
 
